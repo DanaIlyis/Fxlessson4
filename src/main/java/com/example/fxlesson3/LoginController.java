5b33e0fe24c9ee7ccc1f695e1b4f8186d5190ffa
Danallyis <uldana2005@list.ru>
2022-09-27 10:09:56 +0600

    fxLesson3

--- src/main/java/com/example/fxlesson3/LoginController.java
+++ src/main/java/com/example/fxlesson3/LoginController.java
@@ -4,37 +4,52 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
+import javafx.scene.control.TextField;
+
+import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LoginController {
-    @FXML
-    TextField emailTextField, passwordTextField;
+
     @FXML
     Button loginButton, registrationButton;
     @FXML
-    Label incorrectLabel;
+    TextField emailtext , passwordtext;
 
-    public void loginButtonClicked(ActionEvent event){
-        loginButton.setText("Clicked");
+    public void loginbuttonClicked(ActionEvent event){
+        if (emailtext.getText().equals("dana@gmail.com") && (passwordtext.getText().equals("12345"))) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("welcome.fxml"));
+                Scene scene = new Scene(fxmlLoader.load(), 370, 574);
+                Stage stage = new Stage();
+                stage.setTitle("WELCOME PAGE");
+                stage.setScene(scene);
+                stage.show();
+                ((Node)(event.getSource())).getScene().getWindow().hide();
+            }
+            catch (IOException e) {
+                System.out.println(e.toString());
+            }
+        }
     }
 
-    public void registrationButtonClicked(ActionEvent event) {
+    public void registrationbuttonClicked(ActionEvent event){
         try {
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setLocation(getClass().getResource("registration.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(),600, 400);
+            Scene scene = new Scene(fxmlLoader.load(), 370, 574);
             Stage stage = new Stage();
-            stage.setTitle("Registration form");
+            stage.setTitle("REGISTRATION PAGE");
             stage.setScene(scene);
             stage.show();
             ((Node)(event.getSource())).getScene().getWindow().hide();
-        }catch (IOException e){
+        }
+        catch (IOException e) {
             System.out.println(e.toString());
         }
     }
